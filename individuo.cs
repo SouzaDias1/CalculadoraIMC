85723dcfdb84d0acc86b2fc01d4c128999415f91
Pedro Henrique <phdias2004@gmail.com>
2022-04-05 18:36:28 -0300

    inserção da classe individuo

---
+++ individuo.cs
@@ -0,0 +1,12 @@
+public class individuo
+{
+    public string nome { get; set; }
+    public float altura { get; set; }
+    public float peso { get; set; }
+    public double imc {get => peso / (altura * altura); }
+
+    public Individuo(double )
+    {
+        
+    }
+}
